07bae0dfffb1810c2fbf6d0606087bbf75f9414f
Swaraj Dangare <51025421+SwarajDangare@users.noreply.github.com>
2022-12-18 16:05:24 +0530

    Added a file to imu_6050

--- DUMMY ARM/imu_6050/imu_6050_yaw.ino
+++ DUMMY ARM/imu_6050/imu_6050_yaw.ino
@@ -21,9 +21,9 @@ void loop()
 {
 
   Wire.beginTransmission(MPU);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.write(0x3B);                // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
+  Wire.requestFrom(MPU,14,true);   // request a total of 14 registers
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
